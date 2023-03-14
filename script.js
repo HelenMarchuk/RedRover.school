8a566e57c9901f71475cf7015654b3cc69766248
Olena Marchuk <helen44marchuk@gmail.com>
2023-03-14 11:10:15 +0200

    Add discription for tasks

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Part-1
+// 1.1 Объявите две переменные: firstName и lastName.
+// 1.2 Передайте значение “John”в переменную firstName и значение “Adams”  в переменную lastName.
+// 1.3 Выведите на страницу значение firstName и lastName (должно показать John Adams).
+// 1.4 Используйте те же переменные (перезапишите) firstName и lastName для того, чтобы вывести новое значение (Anna Karenina).
+// В итоге на экране должно быть две строки:
+// John Adams
+// Anna Karenina
+
 let firstName
 let lastName
 firstName = "John"
@@ -10,26 +18,37 @@ lastName = "Karenina"
 document.write(firstName + " " + lastName,"<br><br>")
 
 // Part-2
+// 2  Выведите в консоль тип данных следующих переменных:
+// let a,
+// let c = 9,
+// let str = “Hi 5!”,
+// let b = true,
+// let y = 9 + ‘1’,
+// let x =  'a' / 6
+
 let a
 let c = 9
 let str = "Hi 5"
 let b = true
 let y = 9+'1'
 let x = 'a'/6
-console.log(a)
-console.log(c)
-console.log(str)
-console.log(b)
-console.log(y)
-console.log(x)
+console.log(typeof a)
+console.log(typeof c)
+console.log(typeof str)
+console.log(typeof b)
+console.log(typeof y)
+console.log(typeof x)
 
 // Part-3
+// 3 Подумайте, какие переменные логично объявить через const. Придумайте 3 разных,  объявите их и  выведите на экран
+
 const lion = "animal"
 const blackColor = "000"
 const myBirthday = "04.01.1988"
 document.write(lion + "<br>" + blackColor + "<br>" + myBirthday + "<br><br>")
 
 // Creative task
+// 4 Напишите пошаговый алгоритм приготовления кофе и выведите его на экран (страницу).
 document.write("Instruction of making a coffee"+ "<br><br>" + "What do you need:"+ "<br>")
 
 let machine = "coffee machine"
@@ -48,12 +67,17 @@ let step7 = `7.Turn off ${machine}`
 document.write(step1 + "<br>" + step2 + "<br>" + step3 + "<br>" + step4 + "<br>" + step5 + "<br>"               + step6 + "<br>" + step7 + "<br><br>")
 
 // Review1-part1
+// Задача 1 Напишите формулу, которая вычисляет BMI (Body Mass Index), 
+// BMI = вес (кг)/ рост (м) в квадрате. Напечатайте результат в консоли. Выведите вместе сo своим именем на страницу
+
 let weight = 64
 let height = 1.74
 document.write("Olena's BMI = ", weight/height**2, "<br><br>")
 console.log(weight/(height**2))
 
 // Review1-part2
+// Задача 2. Создайте две переменные с любыми значениями. Напишите программу, которая поменяет эти значения местами. Update. То же задание с числовыми переменными. Будет ли разница?
+  
 let berry = "strawberry"
 let fruit = "lemon"
 let food = berry
